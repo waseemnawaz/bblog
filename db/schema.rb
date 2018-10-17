88628560d4f2c5a9308e1f243e90d29b364d18e4
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-10-17 16:19:23 +0200

    migration to add link to users web

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_143349) do
+ActiveRecord::Schema.define(version: 2018_10_16_194734) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_10_14_143349) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
 end
