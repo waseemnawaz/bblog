1e3d51a109694c1f13f13201f10e38527dc5f18a
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-10-19 21:46:48 +0200

    gem cancancan and abilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+	rescue_from CanCan::AccessDenied do |exception|
+  	redirect_to main_app.root_url, alert: exception.message
+	end
 
 	def after_sign_in_path_for(resource)
 		user_path(resource)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
+    can :manage, Project, user_id: user.id
+    can :read, :all
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
