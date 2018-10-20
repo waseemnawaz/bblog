340b0f080eee41578051bd30e6dafe24298bb7f5
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-10-20 16:02:47 +0200

    validating image_url input

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   belongs_to :user
+  validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
 end
