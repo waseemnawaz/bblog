d6d86a2a625f4ba62c175dd23fc53db927c0d5e1
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-10-22 16:38:53 +0200

    temporarly change validation in projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
-  validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
+  #this enables people to only add images. 
+  #validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
 end
